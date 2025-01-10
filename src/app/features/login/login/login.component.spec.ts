93e2c0b754ffc950076262f53337180bec7fd12c
urseacristian <80846706+urseacristian@users.noreply.github.com>
2025-01-10 20:56:56 +0200

    last

--- src/app/features/login/login/login.component.spec.ts
+++ src/app/features/login/login/login.component.spec.ts
@@ -1,47 +1,23 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { BackendService } from '../../../service/services/backend.service';
-import { AuthService } from '../../../service/auth/auth.service';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-@Component({
-  selector: 'app-login',
-  standalone: false,
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
-})
-export class LoginComponent {
-  loginForm: FormGroup;
+import { LoginComponent } from './login.component';
 
-  constructor(
-    private fb: FormBuilder,
-    private backendService: BackendService,
-    private authService: AuthService,
-    private router: Router
-  ) {
-    this.loginForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required],
-    });
-  }
+describe('LoginComponent', () => {
+  let component: LoginComponent;
+  let fixture: ComponentFixture<LoginComponent>;
 
-  onSubmit(): void {
-    if (this.loginForm.valid) {
-      this.backendService.loginUser(this.loginForm.value).subscribe({
-        next: (response: any) => {
-          if (response && response.token) {
-            const profile = { email: this.loginForm.value.email, ...response.user };
-            this.authService.login(response.token, profile);
-            this.router.navigate(['/home']);
-          } else {
-            alert('Invalid credentials');
-          }
-        },
-        error: (err) => {
-          console.error(err);
-          alert('Login failed');
-        },
-      });
-    }
-  }
-}
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [LoginComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(LoginComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
